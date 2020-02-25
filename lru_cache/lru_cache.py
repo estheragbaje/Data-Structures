2cb6bc208be5218a5f2adedb667b0707f1fa3aa4
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-25 22:20:03 +0100

    first attempt at lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,15 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        # state the limit of node the lru cache can hold
+        self.limit = limit
+        # create a linked list to hold the key value pair
+        self.storageList = DoublyLinkedList()
+        # track the current number of node it is holding
+        self.currentNode = 0
+        # create a storage dict for fast acccess to node in the cache
+        self.cacheStorage = {}
+
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +27,16 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        # if the key does not exist in the cache_storage return None
+        if key not in self.cacheStorage:
+            return None
+        # otherwise return the value associated with the key and move the node to the head of the storage List
+        else:
+            node = self.cacheStorage[key]["node"]
+            self.storageList.move_to_front(node)
+            
+            return self.cacheStorage[key]["value"]
+
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +49,24 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        # if the key exist in the cache storage, replace its value and move the node to the head in the storage list
+        if key in self.cacheStorage:
+          self.cacheStorage[key]["value"] = value
+          key_node = self.cacheStorage[key]["node"]
+          self.storageList.move_to_front(key_node)
+        else:
+        # check if the length of the node is up to the limit
+            if self.currentNode == self.limit:
+        #if it is, delete the tail of the cache_list and remove the key value pair from the cache_storage
+                popped_key = self.storageList.remove_from_tail()
+                self.cacheStorage.pop(popped_key)
+        # add the new key value pair to the cache_storage and cache_list head
+                node = self.storageList.add_to_head(key)
+                self.cacheStorage[key] = {"value": value, "node": node}
+        # if the length is not up to the limit, simply add the new key value pair to the cache_storage and cache_list head and increment node_count
+            else:
+                node = self.storageList.add_to_head(key)
+                self.cacheStorage[key] = {"value": value, "node": node}
+                self.currentNode += 1
+
+      
--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,6 +7,7 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        #Because it can be traversed in two ways
         self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
