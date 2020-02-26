0b0fb27d79277fda2682983ad18fd346ae9a1463
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-26 20:47:19 +0100

    complete lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,24 @@
+"""
+(value, ref-next) ==> SinglelyLL
+(ref-prev, value, ref-next) == DLL
+slots (linked list) ==> []*3
+cache {} ==> {key, values}
+input format = (key, value)
+input((1, 0)) [(1, 0)]
+input((2, 4)) [(2, 4),(1, 0)]
+input((3, 6)) [(3, 6), (2, 4), (1, 0)]
+input((5, 9)) [(5, 9), (3, 6), (2, 4)] --1
+input((2, 2)) [(2, 2), (5, 9), (3, 6)]
+
+///
+arrA = [1, 3, 4, 7]
+arrA[2] = 4
+for i in range(len(arrA)):
+    arrA[i]
+
+node1<==>node2<==>node3
+node1.next = node2
+"""
 from doubly_linked_list import DoublyLinkedList
 
 class LRUCache:
@@ -30,12 +51,15 @@ class LRUCache:
         # if the key does not exist in the cache_storage return None
         if key not in self.cacheStorage:
             return None
-        # otherwise return the value associated with the key and move the node to the head of the storage List
+        # otherwise return the value associated with the key and move its node to the head of the storage List
         else:
-            node = self.cacheStorage[key]["node"]
-            self.storageList.move_to_front(node)
-            
-            return self.cacheStorage[key]["value"]
+            """
+            {key: (ref-prev, value=(key, item), ref-next)}
+            self.cacheStorage[key] = (ref-prev, value, ref-next)
+            """
+            node = self.cacheStorage[key]
+            self.storageList.move_to_front(node)       
+            return node.value[1]
 
 
     """
@@ -51,22 +75,24 @@ class LRUCache:
     def set(self, key, value):
         # if the key exist in the cache storage, replace its value and move the node to the head in the storage list
         if key in self.cacheStorage:
-          self.cacheStorage[key]["value"] = value
-          key_node = self.cacheStorage[key]["node"]
-          self.storageList.move_to_front(key_node)
+            # get node stored in dictionary with key ref
+            node = self.cacheStorage[key]
+            # update/overwrite the value of the node stored in the dictionary
+            node.value = (key, value)
+            self.storageList.move_to_front(node)
         else:
         # check if the length of the node is up to the limit
             if self.currentNode == self.limit:
         #if it is, delete the tail of the cache_list and remove the key value pair from the cache_storage
-                popped_key = self.storageList.remove_from_tail()
-                self.cacheStorage.pop(popped_key)
+        # delete item from dictionary
+                del self.cacheStorage[self.storageList.tail.value[0]]
+                # delete node from DLL
+                self.storageList.remove_from_tail()
         # add the new key value pair to the cache_storage and cache_list head
-                node = self.storageList.add_to_head(key)
-                self.cacheStorage[key] = {"value": value, "node": node}
+                node = self.storageList.add_to_head((key, value))
+                self.cacheStorage[key] = self.storageList.head
         # if the length is not up to the limit, simply add the new key value pair to the cache_storage and cache_list head and increment node_count
             else:
-                node = self.storageList.add_to_head(key)
-                self.cacheStorage[key] = {"value": value, "node": node}
+                node = self.storageList.add_to_head((key,value))
+                self.cacheStorage[key] = self.storageList.head
                 self.currentNode += 1
-
-      
